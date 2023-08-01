1fd8ed8d623b19c62aa388b277b44d0eec1094da
DGV98 <davidgonzalez1019@gmail.com>
2023-08-01 03:12:57 -0500

    Added packages

--- backend/api.py
+++ backend/api.py
@@ -1,45 +1,44 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
 from flask_session import Session
 from helpers import login_required
-from math_response import generate_prompt, Problem
+from math_response import generate_prompt, get_response, clean_response
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins":"*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Set session config
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route('/api/login', methods = ["POST"])
-def login():
-    session.clear()
-    if not request.json["user_id"]:
-        return jsonify("No username")
-    session["user_id"] = request.json["user_id"]
 
-    
+# @app.route("/api/login", methods=["POST"])
+# def login():
+#     session.clear()
+#     if not request.json["user_id"]:
+#         return jsonify("No username")
+#     session["user_id"] = request.json["user_id"]
 
-@app.route('/api/generate', methods = ["POST"])
-@login_required
+
+@app.route("/api/test", methods=["POST"])
+def test():
+    return jsonify({"Result": "Success " + request.json["category"]})
+
+
+@app.route("/api/generate", methods=["POST"])
+# @login_required
 def define_problem():
     category = request.json["category"]
     difficulty = request.json["difficulty"]
-    p = Problem(generate_prompt(category, difficulty))
-    session["problem"] = p
-        
-    return jsonify(next(p))
 
-@app.route('/api/next', methods = ["GET"])
-@login_required
-def get_next_problem():
-    return jsonify(next(session["problem"]))
+    return jsonify(clean_response(get_response(generate_prompt(category, difficulty))))
 
-@app.route('/api/logout')
-def logout():
-    session.clear()
-    return jsonify("success")
+
+# @app.route("/api/logout")
+# def logout():
+#     session.clear()
+#     return jsonify("success")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/math_response.py
+++ backend/math_response.py
@@ -1,58 +1,51 @@
 import openai
-import os
-from collections import deque
 import re
-import random
-from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-if not os.environ.get("OPENAI_API_KEY"):
-    raise RuntimeError("API_KEY not set")
-else:
-    openai.api_key = os.environ.get("OPENAI_API_KEY")
+# if not os.environ.get("OPENAI_API_KEY"):
+#     raise RuntimeError("API_KEY not set")
+# else:
+#     openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-class Problem():
-    def __init__(self, prompt):
-        self.q = deque()
-        self.prompt = prompt
-        self.get_response()
+def get_response(prompt) -> str:
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a math professor tutoring one of your students.",
+            },
+            {
+                "role": "user",
+                "content": "Can you generate a list of 5 calculus problems that are of easy difficulty? Please format your problems LaTex format.",
+            },
+            {
+                "role": "assistant",
+                "content": "Here are the problems formatted in LaTex:\n\n1. Find the derivative of the function \\(f(x) = 3x^2 - 4x + 2\\).\n\n2. Evaluate the definite integral \\(\\int_{0}^{4} (2x + 1) \\, dx\\).\n\n3. Determine the limit \\(\\lim_{x \\to 2} \\frac{x^2 - 4}{x - 2}\\).\n\n4. Find the critical points of the function \\(g(x) = x^3 - 6x^2 + 9x\\).\n\n5. Calculate the area bounded by the curve \\(y = x^2\\) and the x-axis in the interval \\([-2, 2]\\).\n\nLet me know if you need any further assistance with these problems!",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.6,
+        max_tokens=400,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    return response["choices"][0]["message"]["content"]
 
-    def __next__(self):
-        if len(self.q) == 1:
-            self.get_response()
-        return self.q.popleft()
 
-    def __iter__(self):
-        return self
-
-    def get_response(self) -> str:
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=self.prompt,
-            temperature=.2,
-            max_tokens=400,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        self.clean_response(response["choices"][0]["text"])
-
-    def clean_response(self, response: str) -> None:
-        response = response.strip().split("\n")
-        # print(response)
-        for problem in response:
-            if not problem:
-                continue
-            problem = problem[3:]
-            self.q.append(problem)
-
-    def __repr__(self) -> str:
-        return f"Questions: {self.q}"
+def clean_response(response: str):
+    q = []
+    pattern = r"^\n((?:\n|.)*?)\n$"
+    matches = re.findall(pattern, response, flags=re.M)
+    for i in matches:
+        q.append(i[3:])
+    return q
 
 
 def generate_prompt(category: str, difficulty: str) -> str:
     if not category or not difficulty:
         raise ValueError("No category or difficulty provided.")
-    return f"Act like you are a math professor tutoring one of your students. Can you generate a list of 5 {category.lower()} problems that are of {difficulty.lower()} difficulty. Please format your problems in a way that can be interpreted by Wolfram Alpha for answers."
+    return f"Can you generate a list of 9 {category.lower()} problems that are of {difficulty.lower()} difficulty? Please format your problems LaTex format."
